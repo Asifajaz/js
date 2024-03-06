e3561ee75a15ea850d78ebff38e4238c877da80d
Asifajaz <159148615+Asifajaz@users.noreply.github.com>
2024-03-06 11:59:19 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,113 @@
+// let fullname = "rebort"
+// console.log (fullname)
+
+// let a : number = 1
+// let b : number = 2
+// console.log ("a + b=", a + b);
+// console.log ("a - b=", a - b);
+// console.log ("a / b=", a / b);
+// console.log ("a * b=", a * b);
+// console.log ("a % b=", a % b);
+// console.log ("a ** b=", a ** b);
+
+
+
+// let fullname = "asif"
+// console.log (fullname)
+
+// let a : number  = 2
+// let b : number  = 2
+// console.log ("a + b =", a + b);
+// console.log ("a - b =" , a - b);
+// console.log ("a * b =", a * b);
+// console.log ("a % b =", a % b);
+// console.log ("a / b=", a / b);
+
+
+
+// let fullname = "lucky"
+// console.log (fullname)
+
+// let a : number = 3
+// let b : number = 3
+// console.log ("a + b =", a + b);
+// console.log ("a - b =", a - b);
+// console.log ("a * b =", a * b);
+// console.log ("a % b =", a % b);
+// console.log ("a / b =", a / b);
+// console.log ("a ** b =", a ** b);
+
+
+
+// let fullname:string =  "Asif-Ajaz"; //char ='a' or "a"  string='a6730580-t93' or "a5684738dijf"
+// char      any =1 . "fdkmr485468595" {} , [residji,ritu834534]
+// myobj={
+// a:nuber . 
+/// b:string="fdkmr485468595" 
+//{} , [residji,ritu834534]
+// }
+
+
+
+
+// console.log (fullname)
+
+// let a: number = 2;
+// let b : number = 2
+// console.log ("a + b =", a + b);
+// console.log ("a - b =", a - b);
+// console.log ("a % b =", a % b);
+// console.log ("a * b =", a * b);
+// console.log ("a / b =", a / b);
+
+
+
+
+
+
+// let massage : string = 'Asif ! ;'
+// console.log (massage)
+// massage = '133'
+// console.log (massage)
+
+
+
+// let fname : string = "asif";
+// console.log ('good day , ' , fname, '!')
+
+
+
+
+// let fname : string = "asif";
+// console.log ('good day , ' +  fname + '!')
+
+
+
+
+// let uname : string = 'ASIF '; // STRING
+// let age : number = 24 ; // number
+// let isStudent : Boolean = false; // boolean
+// let randomvalue : any = 'https://www.fiverr.com/asifajazmugahl?up_rollout=true'; // any
+
+
+
+
+// //{string}
+
+// let a : string = ' helllo'
+// console.log(a)
+
+
+// // {number}
+// let b : number = ' 21'
+// console.log(b)
+
+
+// // {boolean}
+// let c : boolean = true
+// console.log(c)
+
+
+// //{null}
+// let d : null = 'null'
+// console.log(d)
